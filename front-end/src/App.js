36ddfa3e30c607943ac92613b7f89f1ad203cf3f
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-08-22 04:55:19 -0700

    adding front end

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-black">
+        This is new App for Hotel Reservation
+      </div>
+    </div>
+  );
+}
+
+export default App;
