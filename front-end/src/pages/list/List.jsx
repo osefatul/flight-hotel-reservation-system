8aa653ac3d1a257f2c742488f68f947e22970507
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-08-23 23:42:47 -0700

    sidebar for list is done

--- front-end/src/pages/list/List.jsx
+++ front-end/src/pages/list/List.jsx
@@ -10,6 +10,7 @@ import SearchItem from '../../components/SearchItem';
 
 function List() {
     const location = useLocation();
+    
     const [destination, setDestination] = useState(location.state.destination);
     const [date, setDate] = useState(location.state.date);
     const [openDate, setOpenDate] = useState(false);
@@ -25,33 +26,39 @@ function List() {
         </div>
 
         <div className="flex justify-center mt-4">
+
             <div className="w-full flex space-x-3 relative">
 
-                <div className="bg-[#febb02] p-2 rounded-sm sticky z-50 h-max top-10">
-                    <h1 className="text-[##555] mb-3 font-md">Search</h1>
+                <div className="bg-[#febb02] p-2 rounded-sm sticky z-50 h-max top-10 space-y-6">
 
-                    <div className="flex flex-col space-y-4 mb-3">
-                        <label className='text-sm'>Destination</label>
-                        <input className="!border-none !outline-none h-5 bg-yellow-400 rounded-md pl-1" placeholder={destination} type="text" />
+                    <div className='space-y-3'>
+                        <h1 className="text-[##555] text-md font-bold">Search</h1>
+
+                        <div className="flex flex-col space-y-1">
+                            <label className='text-sm'>Destination</label>
+                            <input className="!border-none !outline-none h-5 bg-slate-100 rounded-md pl-1" placeholder={destination} type="text" />
+                        </div>
+
+                        <div className="flex flex-col space-y-1">
+                            <label className='text-sm'>Check-in Date</label>
+                            <input 
+                            type="text"
+                            className="!border-none !outline-none h-5 bg-slate-100 rounded-md pl-1"  
+                            placeholder={`${format(date[0].startDate, "MM/dd/yyyy")}`} />
+                        </div>
+
+                        <div className="flex flex-col space-y-1">
+                            <label className='text-sm'>Check-out Date</label>
+                            <input 
+                            type="text"
+                            className="!border-none !outline-none h-5 bg-slate-100 rounded-md pl-1"  
+                            placeholder={`${format(date[0].endDate, "MM/dd/yyyy")}`} />
+                        </div>
                     </div>
 
-                    <div className="flex flex-col space-y-4 mb-3">
-                        <label>Check-in Date</label>
-                        <span onClick={() => setOpenDate(!openDate)}>{`${format(
-                            date[0].startDate,
-                            "MM/dd/yyyy"
-                        )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
-                        {openDate && (
-                            <DateRange
-                            onChange={(item) => setDate([item.selection])}
-                            minDate={new Date()}
-                            ranges={date}
-                            />
-                        )}
-                    </div>
 
-                    <div className="flex flex-col space-y-4 mb-3">
-                        <label>Options</label>
+                    <div className="flex flex-col space-y-3 mb-3">
+                        <label className='font-bold '>Options</label>
                         
                         <div className="p-2">
 
@@ -59,51 +66,59 @@ function List() {
                                 <span className="lsOptionText">
                                     Min price <small>per night</small>
                                 </span>
-                                <input type="number" className="w-10" />
+                                <input 
+                                type="number"  
+                                className="w-10 pl-1 rounded-sm"
+                                />
                             </div>
                             
                             <div className="flex justify-between text-[#555] text-sm mb-2">
                                 <span className="lsOptionText">
                                     Max price <small>per night</small>
                                 </span>
-                                <input type="number" className="w-10" />
+                                <input 
+                                type="number" 
+                                className="w-10 pl-1 rounded-sm"
+                                />
                             </div>
 
                             <div className="flex justify-between text-[#555] text-sm mb-2">
                                 <span className="lsOptionText">Adult</span>
                                 <input
-                                    type="number"
-                                    min={1}
-                                    className="w-10"
-                                    placeholder={options.adult}
+                                type="number"
+                                min={1}
+                                className="w-10 pl-1 rounded-sm"
+                                placeholder={options.adult}
                                 />
                             </div>
                             
                             <div className="flex justify-between text-[#555] text-sm mb-2">
-
                                 <span className="lsOptionText">Children</span>
                                 <input
-                                    type="number"
-                                    min={0}
-                                    className="w-10"
-                                    placeholder={options.children}
+                                type="number"
+                                min={0}
+                                className="w-10 pl-1"
+                                
+                                placeholder={options.children}
                                 />
                             </div>
                             
                             <div className="flex justify-between text-[#555] text-sm mb-2">
-
                                 <span className="lsOptionText">Room</span>
                                 <input
                                 type="number"
                                 min={1}
-                                className="w-10"
+                                className="w-10 pl-1"
                                 placeholder={options.room}
                             />
                             </div>
+
                         </div>
                     </div>
                     
-                    <button>Search</button>
+                    
+                    <button className='w-full flex items-center justify-center bg-slate-900 text-white rounded-sm'>Search</button>
+                    
 
                 </div>
             
