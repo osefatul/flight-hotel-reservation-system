e122698fd452266df589ac52c22c5db77c6d3f96
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-08-28 21:46:09 -0700

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,14 @@ const morgan = require("morgan");
 const app = express();
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
+
+const authRoute = require("./routes/auth.js")
+// const usersRoute = require( "./routes/users.js");
+// const hotelsRoute = require( "./routes/hotels.js");
+// const roomsRoute = require("./routes/rooms.js");
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan("combined"));
 //API security
 app.use(helmet());
-// Set body bodyParser
+app.use(cookieParser());
+app.use(cors())
+
+// Set body bodyParser: In order to get access to the post data we have to use body-parser . Basically what the body-parser is which allows express to read the body and then parse that into a Json object that we can understand.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -23,9 +33,10 @@ mongoose.connect(process.env.MONGO_URL).then(
 ).catch(err => console.error(err));
 
 
-app.get("/", (req, res) => {
-    res.json("Hi man")
-})
+app.use("/v1/auth", authRoute);
+// app.use("/v1/users", usersRoute);
+// app.use("/v1/hotels", hotelsRoute);
+// app.use("/v1/rooms", roomsRoute);
 
 
 app.listen(PORT, ()=>{
