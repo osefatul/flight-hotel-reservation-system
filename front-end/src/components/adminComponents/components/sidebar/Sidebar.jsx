91e75c533ae5dd4a655335efee097ac86b1c3046
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-09-26 23:44:05 -0700

    redesgined admin page

--- front-end/src/components/adminComponents/components/sidebar/Sidebar.jsx
+++ front-end/src/components/adminComponents/components/sidebar/Sidebar.jsx
@@ -13,7 +13,12 @@ import {
   Timeline,
   TrendingUp,
   WorkOutline,
-  Room
+  Room,
+  Flight,
+  HolidayVillage,
+  MenuBook,
+  ProductionQuantityLimits,
+  AddBusiness
 } from "@mui/icons-material";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -32,9 +37,15 @@ function Sidebar() {
   const tabs = [
     {id:0, title:"Home", url:"" ,Icon: LineStyle },
     {id:1, title:"Users", url:"users", Icon:PermIdentity}, 
-    {id:2, title:"Hotels", url:"hotels", Icon:Room},
-    {id:3, title:"Room", url:"rooms", Icon: Storefront},
-    {id:4, title:"Reserved", url:"reserved-rooms", Icon: Timeline}
+    {id:2, section:"Stays", title:"Hotels", url:"hotels", Icon:HolidayVillage},
+    {id:3, title:"Rooms", url:"rooms", Icon: Storefront},
+    {id:4, title:"Reserved Rooms", url:"reserved-rooms", Icon: AddBusiness},
+    {id:6, section:"Travel", title:"Flights", url:"flights", Icon: Flight},
+    {id:7, title:"Bookings", url:"bookings", Icon: MenuBook},
+    {id:8, section:"Cart", title:"Orders", url:"orders", Icon: ProductionQuantityLimits}
+
+
+
 ] 
 
 
@@ -45,16 +56,18 @@ const handleSubmit =  (index, url) =>{
   }
   return (
 
-    <div className="homeHeight flex flex-col sticky z-50 top-[50px]">
-          <h3 className="px-2 text-gray-400 font-bold pb-4">Dashboard</h3>
-          <div className='flex flex-col space-y-4 text-sm '>
+    <div className="homeHeight pb-4 flex flex-col sticky z-50 top-[70px]">
+          <h1 className="px-2 text-xl text-slate-700 font-bold pb-4">Dashboard</h1>
+          <div className='flex flex-col space-y-2 text-sm '>
             {tabs.map((tab ,index)=>(
+              <div>
+                {"section" in tab? 
+                <h2 className="px-2 text-gray-400 font-bold pb-1">{tab.section}</h2> 
+                : " "}
                 <motion.div 
-                key={index}
-                
-                className={`${index === selected && " border-l-4 border-blue-800 bg-slate-300 hover:shadow-md  "} w-full h-8 flex text-[12px] items-center justify-start px-2 hover:bg-slate-300 hover:shadow-md ` }
-                onClick={() => handleSubmit(index, tab.url)}
-                >
+                  key={index}
+                  className={`${index === selected && " border-l-4 border-blue-800 bg-slate-300 hover:shadow-md  "} w-full h-8 flex text-[10px] items-center justify-start px-2 hover:bg-slate-300 hover:shadow-md ` }
+                  onClick={() => handleSubmit(index, tab.url)}>
                     <motion.p 
                     className=" cursor-pointer"
                     whileHover={{ scale: 1.03 }}
@@ -63,8 +76,11 @@ const handleSubmit =  (index, url) =>{
                       <tab.Icon/> {tab.title}
                     </motion.p>
                 </motion.div>
+              </div>
+
             ))}
           </div>
+          <h2></h2>
     </div>
 
     )
